8a4a1c6c7de9aea430fb7e3f14154fd6b45798ef
veronica romero <vdrr89@gmail.com>
2021-05-26 23:49:55 -0300

    primera parte de Crear Producto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,14 @@ import { CajaComponent } from './caja/caja.component';
 import { InicioComponent } from './inicio/inicio.component';
 import { ProductoCrudComponent } from './producto-crud/producto-crud.component';
 import { ProductoDetalleComponent } from './producto-detalle/producto-detalle.component';
+import { CrearProductoComponent } from './crear-producto/crear-producto.component';
 
 const routes: Routes = [
   {path:'',component: InicioComponent},
   {path:'caja',component: CajaComponent},
   {path:'producto/:id',component:ProductoDetalleComponent},
-  {path:'productos',component:ProductoCrudComponent}
+  {path:'productos',component:ProductoCrudComponent}, 
+  {path:'crear-producto',component:CrearProductoComponent}
 ]
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
